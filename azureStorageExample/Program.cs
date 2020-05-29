0bf5d35b53a381780c013f540089b812c50f5477
ian simpson <iansimpson55@hotmail.com>
2020-05-29 10:53:11 +0100

    Add project files.

---
+++ azureStorageExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azureStorageExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
